1abf698a6bb3b34c3ee4bcaf66ca8a63d0d773db
Umid <umid010699@mail.ru>
2021-01-22 19:25:58 +0500

    Firebase auth & redux thunk connected

---
+++ src/controllers/constants/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDg0KTwD_xQuADKGEv69TbE13a8CJWfBRo",
+  authDomain: "fir-auth-e4d15.firebaseapp.com",
+  projectId: "fir-auth-e4d15",
+  storageBucket: "fir-auth-e4d15.appspot.com",
+  messagingSenderId: "862540289648",
+  appId: "1:862540289648:web:a4f8ca57d69f471b4786b5"
+}
